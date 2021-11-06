f4abf1384a65c9cc96f30e686c862ab1a4d71a90
Bolaji Ayodeji <sirbeejay1@gmail.com>
2021-11-06 08:22:44 +0100

    chore: update requirements

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import flask
 import pickle
-import pandas as pd
 
 # Use pickle to load in the pre-trained model.
 with open(f'model/diabetes.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = flask.Flask(__name__, template_folder='templates')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
